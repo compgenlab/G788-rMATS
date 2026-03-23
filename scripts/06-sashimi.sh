01b7806bb30f48cec10a00af70d5601a3368bc32
Marcus R. Breese <marcus@breese.com>
2026-03-23 17:09:24 -0400

    plot more

--- scripts/06-sashimi.sh
+++ scripts/06-sashimi.sh
@@ -3,11 +3,11 @@
 # Get the header plus your top hits by |ΔPSI|
 head -1 rmats_output/SE.MATS.JCEC.txt > SE_interesting.txt
 
-# Append top 10 most significant high-delta events
+# Append top 50 most significant high-delta events
 awk -F'\t' 'NR>1 && $20 < 0.05 && ($22 > 0.3 || $22 < -0.3)' \
   rmats_output/SE.MATS.JCEC.txt | \
   sort -t$'\t' -k22 -g | \
-  head -10 >> SE_interesting.txt
+  head -50 >> SE_interesting.txt
 
 rmats2sashimiplot \
   --b1 skin.txt \
